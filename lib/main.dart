becc3917f1cc572ddd483a973355855ccb26f15b
masam0 <masaya.esp@gmail.com>
2023-01-18 22:07:23 +0900

    Chapter3 incomplete but ok

--- lib/main.dart
+++ lib/main.dart
@@ -7,62 +7,68 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Generated App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
         primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
         canvasColor: const Color(0xFFfafafa),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(secondary: const Color(0xFF2196f3)),
       ),
-      home: new MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static var _message = 'ok.';
+  static final _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('App Name'),
-      ),
-      body: new Column(
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('App Name'),
+        ),
+        body: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            new Text(
-              "One",
-              style: new TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Roboto"),
-            ),
-            new Text(
-              "Two",
-              style: new TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Roboto"),
-            ),
-            new Text(
-              "Three",
-              style: new TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Roboto"),
-            )
-          ]),
-    );
+            Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  _message,
+                  style: const TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Roboto"),
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  onChanged: textInput,
+                  controller: _controller,
+                  style: const TextStyle(
+                      fontSize: 28.0,
+                      color: Color(0xffFF0000),
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Roboto"),
+                )),
+          ],
+        )));
+  }
+
+  void textInput(String text) {
+    setState(() {
+      _message = 'you are saying "$text"';
+    });
   }
 }
