3b593af1c15ce69d7dfb42b5d1c602e0c67d0270
masam0 <masaya.esp@gmail.com>
2023-01-18 21:15:29 +0900

    Chapter2-End

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
+      ),
+      body: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "One",
+              style: new TextStyle(
+                  fontSize: 32.0,
+                  color: const Color(0xFF000000),
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Roboto"),
+            ),
+            new Text(
+              "Two",
+              style: new TextStyle(
+                  fontSize: 32.0,
+                  color: const Color(0xFF000000),
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Roboto"),
+            ),
+            new Text(
+              "Three",
+              style: new TextStyle(
+                  fontSize: 32.0,
+                  color: const Color(0xFF000000),
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Roboto"),
+            )
+          ]),
+    );
+  }
+}
